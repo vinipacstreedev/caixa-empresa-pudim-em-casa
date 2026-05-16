a1cffdfcd19a0977a1bb2b81523111d2282eedd7
Vinicius Pereira <viniciuss.ssouzaa@gmail.com>
2026-05-16 08:49:46 -0300

    First commit

---
+++ src/test/java/com/pudim/caixa/CaixaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pudim.caixa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
