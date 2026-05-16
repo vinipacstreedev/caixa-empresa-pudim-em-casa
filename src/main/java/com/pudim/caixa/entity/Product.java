8fc6b7dda6e9bed58372dbce7541613cabb01dc2
Vinicius Pereira <viniciuss.ssouzaa@gmail.com>
2026-05-16 09:13:52 -0300

    Class product

---
+++ src/main/java/com/pudim/caixa/entity/Product.java
@@ -0,0 +1,23 @@
+package com.pudim.caixa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "produto")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private Integer stock;
+}
